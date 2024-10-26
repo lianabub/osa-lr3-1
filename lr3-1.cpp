285ce8ba3f2ece34e593f4ac4c7111fe7b52ae6b
liana <lianabub07@gmail.com>
2024-10-26 09:08:39 +0300

    Add files via upload

---
+++ lr3-1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+	int a, b, c, D, x1, x2;
+
+	cout << "Введите коофиценты ";
+	cin >> a >> b >> c;
+	D = b * b - (4 * a * c);
+	if (D > 0) {
+		cout << "Нет корней";
+	}
+	else if (D > 0) {
+		x1 = -b / 2 * a;
+		cout << "Существует только один корень" << x1;
+	else {
+		x1 = (-b + sqrt(D)) / 2 * a;
+		x2 = (-b - sqrt(D)) / 2 * a;
+		cout << "Корни этого уравнения " << x1 << x2;
+	     }
+	}
+
+}
