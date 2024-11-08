f750e03b954f936b7765623eee914584e71de204
liana <lianabub07@gmail.com>
2024-11-08 11:49:13 +0300

    Update lr3-1.cpp

--- lr3-1.cpp
+++ lr3-1.cpp
@@ -1,23 +1,26 @@
-﻿#include <iostream>
-#include <math.h>
-using namespace std;
-int main() {
-	int a, b, c, D, x1, x2;
+g namespace std;
 
-	cout << "Введите коофиценты ";
-	cin >> a >> b >> c;
-	D = b * b - (4 * a * c);
-	if (D < 0) {
-		cout << "Нет корней";
-	}
-	else if (D = 0) {
-		x1 = -b / 2 * a;
-		cout << "Существует только один корень" << x1;
-	else {
-		x1 = (-b + sqrt(D)) / 2 * a;
-		x2 = (-b - sqrt(D)) / 2 * a;
-		cout << "Корни этого уравнения " << x1 << x2;
-	     }
-	}
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    int a, b, c, D, x1, x2;
 
-}
+    cout << "Введите коофиценты ";
+    cin >> a >> b >> c;
+    D = b * b - (4 * a * c);
+    if (D < 0)
+    {
+        cout << "Нет корней";
+    }
+    else if (D = 0)
+    {
+        x1 = -b / 2 * a;
+        cout << "Существует только один корень" << x1;
+    }
+    else
+    {
+        x1 = (-b + sqrt(D)) / 2 * a;
+        x2 = (-b - sqrt(D)) / 2 * a;
+        cout << "Корни этого уравнения " << x1 << x2;
+    }
+    }
